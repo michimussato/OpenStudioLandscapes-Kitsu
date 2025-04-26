8dd6ab7d0669847a47c49bc3c268aa9723f6c0a8
Michael Mussato <michimussato@gmail.com>
2025-04-26 23:10:51 +0200

    refactor

--- noxfile.py
+++ noxfile.py
@@ -1485,10 +1485,7 @@ def dagster_postgres_up(session):
     # nox --session dagster_postgres_up
     # nox --tags dagster_postgres_up
 
-    # if not yml_dagster_postgres.exists():
     write_dagster_postgres_yml()
-
-    # if not compose_dagster_postgres.exists():
     write_dagster_postgres_compose()
 
     session.run(
@@ -1556,10 +1553,7 @@ def dagster_postgres_up_detach(session):
     # nox --session dagster_postgres_up_detach
     # nox --tags dagster_postgres_up_detach
 
-    # if not yml_dagster_postgres.exists():
     write_dagster_postgres_yml()
-
-    # if not compose_dagster_postgres.exists():
     write_dagster_postgres_compose()
 
     session.run(
