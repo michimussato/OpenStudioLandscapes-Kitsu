daac584b0fb8548e188022a6fc99f9a2b6b3ac7f
Michael Mussato <michimussato@gmail.com>
2025-05-27 14:44:29 +0200

    refactor

--- docs/conf.py
+++ docs/conf.py
@@ -249,7 +249,14 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ("index", "user_guide.tex", "OpenStudioLandscapes-Kitsu Documentation", "Michael Mussato", "manual")
+    (
+        # Todo
+        "index",
+        "user_guide.tex",
+        "OpenStudioLandscapes-Kitsu Documentation",
+        "Michael Mussato",
+        "manual",
+    )
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
