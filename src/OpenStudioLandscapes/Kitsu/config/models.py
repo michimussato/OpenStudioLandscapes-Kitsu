69f077818349e90d84f41a9f6c7e7dfcb41fb9a0
Michael Mussato <michimussato@gmail.com>
2025-12-09 03:10:10 +0100

    47-refactor-to-config-based-landscapes: wip

--- src/OpenStudioLandscapes/Kitsu/config/models.py
+++ src/OpenStudioLandscapes/Kitsu/config/models.py
@@ -1,4 +1,5 @@
 import pathlib
+import textwrap
 
 from pydantic import (
     Field,
@@ -8,12 +9,63 @@ from pydantic import (
 )
 
 from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
-from OpenStudioLandscapes.Kitsu.config import dist
+from OpenStudioLandscapes.Kitsu import dist
+
+
+CONFIG_STR = textwrap.dedent(
+    """
+    # Base Information
+    group_name: "Kitsu"
+    key_prefixes:
+      - "Kitsu"
+    #    "__".join(context.asset_key_for_output("DOCKER_COMPOSE").path),
+    #    "docker_compose",
+    #    "docker-compose.yml",
+    
+    # The default Admin user name
+    # Todo:
+    #  - [x] Report Kitsu bug:
+    #        https://github.com/cgwire/zou/issues/960
+    #        Not OK:
+    #        (env) root@kitsu:/opt/zou# zou create-admin --password openstudiolandscapes kitsu@openstudiolandscapes.com
+    #        Email is not valid.
+    #        OK:
+    #        (env) root@kitsu:/opt/zou# zou create-admin --password openstudiolandscapes kitsu@openstudio.com
+    #        Admin successfully created.
+    ## Changing this value does not seem to have an effect
+    #kitsu_admin_user: admin@example.com
+    ## Changing this value does not seem to have an effect
+    #kitsu_db_password: mysecretpassword
+    kitsu_postgres_conf: "{DOT_FEATURES}/{FEATURE}/.payload/config/etc/postgresql/14/main/postgresql.conf"
+    
+    # https://zou.cg-wire.com/jobs/#enabling-job-queue
+    kitsu_enable_job_queue: true
+    kitsu_port_container: 80
+    kitsu_port_host: 4545
+    
+    # Inside Landscape (ephemeral):
+    kitsu_database_install_destination: "{DOT_LANDSCAPES}/{LANDSCAPE}/{FEATURE}/data/kitsu"
+    # In Landscapes root:
+    #kitsu_database_install_destination: "{DOT_LANDSCAPES}/{DOT_SHARED_VOLUMES}/{GROUP}__{KEY}/data/kitsu"
+    kitsu_preview_folder: /opt/zou/previews
+    kitsu_secret_key: yourrandomsecretkey
+    kitsu_tmp_dir: /opt/zou/tmp
+    # these options will be dropped as they don't match the BaseModel
+    illegal_option: "i will be ignored"
+    ignored_option: "i will be ignored"
+    
+    # Todo
+    #  - [ ] define arbitrary compose_scope here
+    #compose_scope: default
+    """
+)
 
 
 class Config(FeatureBaseModel):
+    # config_str: str = CONFIG_STR
+
     feature_name: str = dist.name
-    compose_scope: str = "default"
+    # compose_scope: str = "default"
     definitions: str = "OpenStudioLandscapes.Kitsu.definitions"
 
     kitsu_admin_user: EmailStr = Field(
@@ -85,6 +137,14 @@ class Config(FeatureBaseModel):
                 "`kitsu_port_container` must be set "
                 "to 80 for now. Other values will render Kitsu inoperable."
             )
+
+    # def export(self, destination: pathlib.Path):
+    #
+    #     with destination.open("w") as f:
+    #         f.write(self.yaml(indent=2))
+    #     return {
+    #         "feature_name": self.feature_name,
+    #     }
 
     # @field_validator("kitsu_postgres_conf")
     # @classmethod
