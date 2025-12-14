1e0c40ed021b6fc6a453a3096ba209fa9c009b99
Michael Mussato <michimussato@gmail.com>
2025-12-14 09:44:03 +0100

    47-refactor-to-config-based-landscapes: fully dynamic compose scopes

--- src/OpenStudioLandscapes/Kitsu/assets.py
+++ src/OpenStudioLandscapes/Kitsu/assets.py
@@ -17,7 +17,6 @@ from dagster import (
     Output,
     asset,
 )
-from deepdiff import DeepDiff
 from OpenStudioLandscapes.engine.common_assets.compose import get_compose
 from OpenStudioLandscapes.engine.common_assets.docker_compose_graph import (
     get_docker_compose_graph,
--- src/OpenStudioLandscapes/Kitsu/config/models.py
+++ src/OpenStudioLandscapes/Kitsu/config/models.py
@@ -56,11 +56,13 @@ class Config(FeatureBaseModel):
     kitsu_database_install_destination: pathlib.Path = Field(
         description="The host side Kitsu database installation destination."
     )
-    kitsu_db_inside_container: bool = Field(
-        default=False,
-        description="The Kitsu database inside container; the database will "
-                    "not be persistent. Helpful for testing.",
-    )
+    # Todo:
+    #  - [ ] Implement?
+    # kitsu_db_inside_container: bool = Field(
+    #     default=False,
+    #     description="The Kitsu database inside container; the database will "
+    #                 "not be persistent. Helpful for testing.",
+    # )
     kitsu_preview_folder: pathlib.Path = Field(
         description="The Kitsu Preview folder.",
         default=pathlib.Path("/opt/zou/previews"),
