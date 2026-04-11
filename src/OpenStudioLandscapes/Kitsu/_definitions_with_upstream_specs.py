e31b936c019e70edb7d230cef949fcdb5cde1760
Michael Mussato <michimussato@gmail.com>
2026-04-11 19:07:25 +0200

    Kitsu test successful

---
+++ src/OpenStudioLandscapes/Kitsu/_definitions_with_upstream_specs.py
@@ -0,0 +1,15 @@
+from dagster import Definitions
+from OpenStudioLandscapes.engine.base.assets import group_out_base_spec
+
+from OpenStudioLandscapes.Kitsu.definitions import assets_base
+
+assets_external = []
+assets_external.append(group_out_base_spec)
+
+
+defs = Definitions(
+    assets=[
+        *assets_base,
+        *assets_external,
+    ],
+)
