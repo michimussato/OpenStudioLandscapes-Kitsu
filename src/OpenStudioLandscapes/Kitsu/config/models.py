40b43651922a5562844a47ee3fab61aca80a094e
Michael Mussato <michimussato@gmail.com>
2026-04-26 21:07:49 +0200

    remove compute_fields. serializing them causes problems and is also not needed.

--- src/OpenStudioLandscapes/Kitsu/config/models.py
+++ src/OpenStudioLandscapes/Kitsu/config/models.py
@@ -8,7 +8,7 @@ from pydantic import (
     Field,
     PositiveInt,
     field_validator,
-    computed_field,
+    # computed_field,
 )
 
 LOGGER = get_dagster_logger(__name__)
@@ -940,7 +940,7 @@ class Config(FeatureBaseModel):
             )
 
     # EXPANDABLE PATHS
-    @computed_field
+    # @computed_field
     @property
     def kitsu_database_install_destination_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
@@ -960,7 +960,7 @@ class Config(FeatureBaseModel):
         )
         return ret
 
-    @computed_field
+    # @computed_field
     @property
     def kitsu_preview_folder_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
@@ -980,7 +980,7 @@ class Config(FeatureBaseModel):
         )
         return ret
 
-    @computed_field
+    # @computed_field
     @property
     def kitsu_tmp_dir_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
