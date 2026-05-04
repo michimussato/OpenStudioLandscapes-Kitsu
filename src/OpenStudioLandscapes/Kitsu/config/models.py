77f95f8e5235879e1f9a52cd9b5462e1825b8c2d
Michael Mussato <michimussato@gmail.com>
2026-05-04 14:28:37 +0200

    nox (readmes failed)

--- src/OpenStudioLandscapes/Kitsu/config/models.py
+++ src/OpenStudioLandscapes/Kitsu/config/models.py
@@ -2,16 +2,14 @@ import pathlib
 import textwrap
 from typing import List
 
+from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
+from OpenStudioLandscapes.engine.logging.loggers import FEATURE_LOGGER as LOGGER
 from pydantic import (
     EmailStr,
     Field,
     PositiveInt,
     field_validator,
 )
-
-from OpenStudioLandscapes.engine.logging.loggers import FEATURE_LOGGER as LOGGER
-
-from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 
 from OpenStudioLandscapes.Kitsu import constants, dist
 
@@ -1000,6 +998,7 @@ if __name__ == "__main__":
     CONFIG_STR = Config.get_docs()
 else:
     import yaml
+
     CONFIG_STR = yaml.dump(
         Config.model_json_schema(mode="serialization"),
     )
